60d1b4c61e6cf51df79fe2787f535e32bd7bc25b
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-07-20 17:06:57 +0530

    updtaes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,12 +5,12 @@ this to Contains the class DBStorage
 
 import models
 from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models.base_model import BaseModel, Base
-from models.city import City
 from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
